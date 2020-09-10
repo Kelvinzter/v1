c6bbc1013aa36746bfffec30c8b4ec177075f996
Kelvin Chua <kfc850@cs.bham.ac.uk>
2020-09-10 09:19:27 +0800

    Updates

---
+++ precache-manifest.a55e3681df64a727198f6d36343d405a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79165610ba0b0432613fe40ec7c04ff4",
+    "url": "/v1/index.html"
+  },
+  {
+    "revision": "8aac0e45c4fd229d4d21",
+    "url": "/v1/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fbe45a54ac793695da27",
+    "url": "/v1/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "8aac0e45c4fd229d4d21",
+    "url": "/v1/static/js/2.bd36af2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/v1/static/js/2.bd36af2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe45a54ac793695da27",
+    "url": "/v1/static/js/main.d93c91f0.chunk.js"
+  },
+  {
+    "revision": "37593ef428d6e4141797",
+    "url": "/v1/static/js/runtime-main.0d000dfe.js"
+  }
+]);
